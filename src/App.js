2592123569d3eb28bbde54553450f024295daf7e
zonglinli1 <zonglin_li@ucsb.edu>
2021-11-05 00:53:46 -0700

    broken our homepage into smaller components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/hompage/homepage.component';
 
 function App() {
   return (
@@ -9,5 +9,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
