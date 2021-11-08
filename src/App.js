cdea4cab730986cb304fbaeb4b205e9ecc1a1056
zonglinli1 <zonglin_li@ucsb.edu>
2021-11-07 23:06:13 -0800

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/hompage/homepage.component';
-
-function App() {
+import {Switch, Route} from 'react-router-dom';
+const Hats = (props) => {
   return (
-    <div>
-      <HomePage/>
-    </div>
+  <div>
+    <h1>Hats</h1>
+    <button onClick={()=> props.history.push('/')}>to homepage</button>
+  </div>
   );
 }
-export default App;
+const Jackets = (props) => {
+  return (
+  <div>
+    <h1>Jackets</h1>
+    <button onClick={()=> props.history.push('/')}>to homepage</button>
+  </div>
+  );
+}
+const Sneakers = (props) => {
+  return (
+  <div>
+    <h1>Sneakers</h1>
+    <button onClick={()=> props.history.push('/')}>to homepage</button>
+  </div>
+  );
+}
+const Women = (props) => {
+  return (
+  <div>
+    <h1>Women</h1>
+    <button onClick={()=> props.history.push('/')}>to homepage</button>
+  </div>
+  );
+}
+const Men = (props) => {
+  return (
+  <div>
+    <h1>Men</h1>
+    <button onClick={()=> props.history.push('/')}>to homepage</button>
+  </div>
+  );
+}
+
+export default function App() {
+  return (
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop/hats' component={Hats}/>
+        <Route exact path='/shop/jackets' component={Jackets}/>
+        <Route exact path='/shop/sneakers' component={Sneakers}/>
+        <Route exact path='/shop/womens' component={Women}/>
+        <Route exact path='/shop/mens' component={Men}/>
+      </Switch>
+  );
+}
--- src/pages/hompage/homepage.component.jsx
+++ src/pages/hompage/homepage.component.jsx
@@ -1,10 +1,26 @@
 import React from "react";
-import './homepage.styles.scss'
+import './homepage.styles.scss';
 import Directory from "../../components/directory/directory.component";
+import { Link } from "react-router-dom";
 
-const HomePage = () =>(
-    <div className = 'homepage'>
-        <Directory/>
-    </div>
-)
+
+class HomePage extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+    componentDidMount(){
+        console.log(this.props);
+    }
+    render(){
+        return (
+            <div className = 'homepage'>
+                <Link to='/topics'>Topics</Link>
+                <Directory/>
+            </div>
+        )
+    }
+    componentWillUnmount(){
+    }
+}
 export default HomePage;
