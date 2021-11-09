c4ca2d791c1c140ba674ec259dd7abe165626ea1
zonglinli1 <zonglin_li@ucsb.edu>
2021-11-08 21:19:07 -0800

    fixed a sign in bug

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './sign-in.styles.scss'
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 
 class SignIn extends React.Component{
@@ -15,9 +15,15 @@ class SignIn extends React.Component{
         }
     }
 
-    handleSubmit = (event) =>{
+    handleSubmit = async (event) =>{
         event.preventDefault();
-        this.setState({email:'', password:''});
+        const {email, password} = this.state;
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email:'', password:''});
+        }catch(error){
+            console.log(error);
+        }
     }
 
     handleChange = (event) =>{
@@ -35,7 +41,7 @@ class SignIn extends React.Component{
                     <FormInput name = 'password' type="password" value={this.state.password} label='Password' handleChange={this.handleChange} required/>
                     <div className='buttons'>
                         <CustomButton type="submit" > Sign in</CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn > Sign in with Google</CustomButton>
+                        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn > Sign in with Google</CustomButton>
                     </div>
                     
                 </form>
